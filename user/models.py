c268dffd4977f92a2241d44e773f5e196d3173a0
EmanueleGurini <emanuele.gurini89@gmail.com>
2025-10-20 12:07:21 +0200

    first commit

---
+++ user/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class User(models.Model): 
+	first_name = models.CharField(max_length=50)
+	last_name = models.CharField(max_length=50)
+	email = models.CharField(max_length=100)
+
+	class Meta:
+		db_table = 'users'
+
+	def __str__(self):
+		return f"{self.first_name} {self.last_name}"
