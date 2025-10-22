a34c1f01c588eb732c1d2a66c138ac4f306dd2e2
EmanueleGurini <emanuele.gurini89@gmail.com>
2025-10-22 22:40:43 +0200

    fix: add unique email check for users

--- user/models.py
+++ user/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class User(models.Model): 
 	first_name = models.CharField(max_length=50)
 	last_name = models.CharField(max_length=50)
-	email = models.CharField(max_length=100)
+	email = models.CharField(unique=True)
 
 	class Meta:
 		db_table = 'users'
